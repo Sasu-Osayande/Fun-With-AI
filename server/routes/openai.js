170c496a4d9168bb28a6b9f36dd2a466ee497063
Sasu Osayande <osayande.sasu@outlook.com>
2022-05-14 15:36:10 -0400

    Add a POST route

--- server/routes/openai.js
+++ server/routes/openai.js
@@ -22,7 +22,7 @@ module.exports = () => {
   router.get('/', async(req, res) => {
   let prompt = `Artist: ${req.search}\n\nLyrics:\n`;
   const gptResponse = await openai.complete({
-    engine: 'text-curie-001',
+    engine: 'curie',
     prompt: prompt,
     maxTokens: 50,
     temperature: 0.7,
@@ -34,5 +34,22 @@ module.exports = () => {
 });
 res.status(200).json({response: `${gptResponse.data.choices[0].text}`})
 });
+
+router.post('/', async(req, res) => {
+  let prompt = `Artist: ${req.search}\n\nLyrics:\n`;
+  const gptResponse = await openai.complete({
+    engine: 'curie',
+    prompt: prompt,
+    maxTokens: 50,
+    temperature: 0.7,
+    topP: 1,
+    presencePenalty: 0,
+    frequencyPenalty: 0.5,
+    bestOf: 1,
+    n: 1
+});
+return res.status(200).json({response: `${gptResponse.data.choices[0].text}`})
+});
+
 return router;
 }
