a9247dc345952cb46a1af9a1756866a985a3dc37
Sasu Osayande <osayande.sasu@outlook.com>
2022-05-14 13:51:58 -0400

    Add a route for the openai

---
+++ server/routes/openai.js
@@ -0,0 +1,38 @@
+// const { Configuration, OpenAIApi } = require("openai");
+
+// const configuration = new Configuration({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
+// const openai = new OpenAIApi(configuration);
+
+// ;(async ()=>{
+// const completion = await openai.createCompletion("text-curie-001", {
+//   prompt: "Hello world",
+// });
+// console.log(completion.data.choices[0].text);
+// })();
+
+const router = require('express').Router();
+
+const OpenAI = require('openai-api');
+const openai = new OpenAI(process.env.OPENAI_API_KEY);
+
+module.exports = () => {
+
+  router.get('/', async(req, res) => {
+  let prompt = `Artist: ${req.search}\n\nLyrics:\n`;
+  const gptResponse = await openai.complete({
+    engine: 'text-curie-001',
+    prompt: prompt,
+    maxTokens: 50,
+    temperature: 0.7,
+    topP: 1,
+    presencePenalty: 0,
+    frequencyPenalty: 0.5,
+    bestOf: 1,
+    n: 1
+});
+res.status(200).json({response: `${gptResponse.data.choices[0].text}`})
+});
+return router;
+}
