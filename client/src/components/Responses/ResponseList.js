59ef8c19113d7fb9c39197bf8f137133e3a22f93
Sasu Osayande <osayande.sasu@outlook.com>
2022-05-18 08:11:50 -0400

    Fix error with response container not persisting after a page refresh, and clean up code

--- client/src/components/Responses/ResponseList.js
+++ client/src/components/Responses/ResponseList.js
@@ -5,36 +5,24 @@ import ResponseListItem from "./ResponseListItem";
 function Responses(props) {
   const { storage, setStorage } = props;
 
-  // useEffect(() => {
-  //   const info = window.localStorage.getItem('FunWithAIStorage');
-  //   if ( info !== null ) setStorage(JSON.parse(info));
-  // }, []);
-
-  // useEffect(() => {
-  //   window.localStorage.setItem('FunWithAIStorage', JSON.stringify(storage));
-  // }, [storage]);
-
-  // const returnedValues = props.returnedValues.map((returnedValue) => {
-  //   return (
-  //     <ResponseListItem
-  //       prompt={returnedValue.prompt}
-  //       response={returnedValue.response}
-  //     />
-  //   );
-  // });
-
   useEffect(() => {
-    if (localStorage.getItem("savedData")) {
-      const savedData = JSON.parse(localStorage.getItem("savedData"));
-      setStorage(savedData);
+    if (localStorage.getItem("inputData")) {
+      const inputData = JSON.parse(localStorage.getItem("inputData"));
+      setStorage(inputData);
     }
   }, []);
 
   return (
     <>
       {storage
-        ? storage.map((x, index) => {
-            return <ResponseListItem key={index} prompt={x.prompt} response={x.response} />;
+        ? storage.map((value, index) => {
+            return (
+              <ResponseListItem
+                key={index}
+                prompt={value.prompt}
+                response={value.response}
+              />
+            );
           })
         : []}
     </>
