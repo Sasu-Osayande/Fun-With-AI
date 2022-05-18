ed45d12a492275fd0db2025f36a2b2306425c1c0
Sasu Osayande <osayande.sasu@outlook.com>
2022-05-18 04:32:07 -0400

    Create component for prompt and response value

---
+++ client/src/components/Responses/ResponseListItem.js
@@ -0,0 +1,23 @@
+import React, { useEffect } from "react";
+import "./ResponseListItem.css";
+
+function Responses(props) {
+  const { prompt, response } = props;
+
+  return (
+    <div className="response-container">
+      <div className="prompt-value">
+        <span>
+          <b>Prompt: </b>
+          {prompt}
+        </span>
+      </div>
+      <span>
+        <b>Response: </b>
+        {response}
+      </span>
+    </div>
+  );
+}
+
+export default Responses;
